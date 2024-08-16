c93e87a681a1756d94a8676cd999826548611388
Kristin198689 <163996836+Kristin198689@users.noreply.github.com>
2024-08-16 10:12:00 +0200

    fse

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/styles.css";
+import Header from "./components/HeaderComponent";
+import Main from "./components/MainComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
